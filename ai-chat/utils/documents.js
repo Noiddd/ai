d27f87abfef67ec743bc3b64e0fe77e77d050e13
Dion Ang <dion@Dions-MacBook-Pro.local>
2023-06-29 15:03:03 +0800

    used retrival chain instead on conversational retrival QA chain

--- ai-chat/utils/documents.js
+++ ai-chat/utils/documents.js
@@ -6,6 +6,13 @@ import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { Document } from "langchain/document";
 import { createClient } from "@supabase/supabase-js";
+import { RetrievalQAChain } from "langchain/chains";
+import { OpenAI } from "langchain/llms/openai";
+
+const supabaseClient = createClient(
+  "https://jacratlygjsoxtsvfmxw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphY3JhdGx5Z2pzb3h0c3ZmbXh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE5MTEwNTUsImV4cCI6MTk5NzQ4NzA1NX0.-TnLhGZJ91rgGj2Mf9kpufhd2zdW7Fb5j2rxNVio_Qc"
+);
 
 export const run = async () => {
   const loader = new PDFLoader(
@@ -24,29 +31,30 @@ export const run = async () => {
     new Document({ pageContent: rawDocs }),
   ]);
 
-  const supabaseClient = createClient(
-    "https://jacratlygjsoxtsvfmxw.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphY3JhdGx5Z2pzb3h0c3ZmbXh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE5MTEwNTUsImV4cCI6MTk5NzQ4NzA1NX0.-TnLhGZJ91rgGj2Mf9kpufhd2zdW7Fb5j2rxNVio_Qc"
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: "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", // In Node.js defaults to process.env.OPENAI_API_KEY
+  });
+
+  const model = new OpenAI({
+    openAIApiKey: "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", // In Node.js defaults to process.env.OPENAI_API_KEY
+  });
+
+  const vectorStore = await SupabaseVectorStore.fromDocuments(
+    docOutput,
+    embeddings,
+    {
+      supabaseClient,
+      tableName: "documents",
+      queryName: "match_documents",
+    }
   );
 
-  const embeddings = new OpenAIEmbeddings({
-    openAIApiKey: "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", // In Node.js defaults to process.env.OPENAI_API_KEY
+  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+  const res = await chain.call({
+    query: "What is HFE",
   });
 
-  await supabaseClient.from("documents").insert({
-    content: docOutput,
-    embeddings,
-  });
-
-  // const vectorStore = await SupabaseVectorStore.fromDocuments(
-  //   docOutput,
-  //   embeddings,
-  //   {
-  //     supabaseClient,
-  //     tableName: "documents",
-  //     queryName: "match_documents",
-  //   }
-  // );
+  console.log({ res });
 };
 
 run();
--- ai-chat/utils/streamResponse.js
+++ ai-chat/utils/streamResponse.js
@@ -1,5 +1,9 @@
 import { BufferMemory } from "langchain/memory";
-import { ConversationChain } from "langchain/chains";
+import {
+  ConversationChain,
+  ConversationalRetrievalQAChain,
+  RetrievalQAChain,
+} from "langchain/chains";
 
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import {
@@ -8,6 +12,10 @@ import {
   MessagesPlaceholder,
   SystemMessagePromptTemplate,
 } from "langchain/prompts";
+import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+
+import { createClient } from "@supabase/supabase-js";
 
 const streamResponse = async (prompt) => {
   // creating a streamable object
@@ -19,7 +27,7 @@ const streamResponse = async (prompt) => {
   const encoder = new TextEncoder();
 
   const model = new ChatOpenAI({
-    openAIApiKey: process.env.OPENAI_API_KEY,
+    openAIApiKey: "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",
     temperature: 0.9,
     modelName: "gpt-3.5-turbo",
     streaming: true,
@@ -42,26 +50,33 @@ const streamResponse = async (prompt) => {
     ],
   });
 
-  const memory = new BufferMemory({
-    returnMessages: true,
-    memoryKey: "history",
-  });
+  // vector query
+  const client = createClient(
+    "https://jacratlygjsoxtsvfmxw.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphY3JhdGx5Z2pzb3h0c3ZmbXh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE5MTEwNTUsImV4cCI6MTk5NzQ4NzA1NX0.-TnLhGZJ91rgGj2Mf9kpufhd2zdW7Fb5j2rxNVio_Qc"
+  );
 
   const chatPrompt = ChatPromptTemplate.fromPromptMessages([
     SystemMessagePromptTemplate.fromTemplate(
       `Answer as concisely as possible and ALWAYS answer in MARKDOWN. Current date: ${new Date()}`
     ),
-    new MessagesPlaceholder("history"),
     HumanMessagePromptTemplate.fromTemplate("{input}"),
   ]);
 
-  const openai = new ConversationChain({
-    memory: memory,
-    prompt: chatPrompt,
-    llm: model,
+  const vectorStore = await SupabaseVectorStore.fromExistingIndex(
+    new OpenAIEmbeddings({
+      openAIApiKey: "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",
+    }),
+    { client, tableName: "documents", queryName: "match_documents" }
+  );
+
+  let chaintest = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+
+  const res = chaintest.call({
+    query: `You are Taco, a helpful assistant and an expert in Singapore's Real Estate industry. ALWAYS answer in MARKDOWN. If you do not know the answer, reply with 'hmm... I'm sorry I do not know the answer to that'. Current date: ${new Date()}.${prompt}`,
   });
 
-  openai.call({ input: prompt });
+  console.log({ res });
 
   return stream.readable;
 };
